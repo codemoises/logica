2f3cc6c81dcc00dd4433c06cc71d4a2ec54adfd6
Moises Cruz <moisesbraga223@gmail.com>
2021-11-10 16:28:00 -0300

    Meu decimo terceiro exercício!
    Uso de vetores!

---
+++ ex013/vetores.js
@@ -0,0 +1,21 @@
+//declaração de vetores
+
+var A = [];
+var B = new Array(5);
+var p;
+
+//alimentando os vetores
+
+A[0] = 1;
+A[1] = 2;
+A[2] = 3;
+
+for (p = 1; p <= 5; p++) {
+    B[p] = parseInt(primpt("Informe o valor da posição " + p));
+
+}
+
+//apresentação dos vetores
+
+document.write("A = " + A + "<br>");
+document.write("B = " + B + "<br>");
